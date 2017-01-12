c7d5551a5dd04ddce62ba5ea002e143b97818059
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-12 17:30:43 +0100

    NDVImap Removed echos, added comments, remove temporary files, show reprojectedNDVI file

--- 04ScriptIngevdMondDavidSwinkels.sh
+++ 04ScriptIngevdMondDavidSwinkels.sh
@@ -1,17 +1,21 @@
-#!bin/data
-echo "Script0rs - Inge/David"
-echo "12 January 2017"
-echo "Calculate LandSat NDVI"
+#!/bin/bash
+
+#Script0rs - Inge/David 12 January 2017
+#Calculate LandSat NDVI#
 clear
+#Create directory
+cd data
+#Copy data
 cp LE71700552001036SGS00_SR_Gewata_INT1U.tif input.tif
-echo "calculate ndvi"
-gdal_calc.py -A $fn --A_band=4 -B input.tif --B_band=3 --outfile=ndvi.tif --calc="(A.astype(float)-B)/(A.astype(float)+B)" --type='Float32'
-echo "resampling the NDVI tif file to pixels of 60m"
-gdal_translate -tr 60 60 ndvi.tif resampledndvi.tif
-echo "reprojecting the resampled file to Lat/Long WGS84"
+#Create NDVI map
+gdal_calc.py -A input.tif --A_band=4 -B input.tif --B_band=3 --outfile=ndvi.tif --calc="(A.astype(float)-B)/(A.astype(float)+B)" --type='Float32'
+#Resample to 60m by 60m pixels
+gdal_translate -tr 60 60 ndvi.tif resampledndvi.tif 
+#Reproject to WGS 84 with lon and lat
 gdalwarp -t_srs EPSG:4326 resampledndvi.tif reprojectedndvi.tif
-echo "look at some histogram statistics"
-gdalinfo -hist -stats ndvi.tif
+#Remove temporary files
+rm -f input.tif
+rm -f ndvi.tif
 rm -f resampledndvi.tif
 
 
