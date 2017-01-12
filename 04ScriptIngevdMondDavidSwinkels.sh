cc8a42b2b7f41f0aff699ab52e0d844fb82003b6
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-12 16:32:15 +0100

    NDVI Decreased number of lines

--- 04ScriptIngevdMondDavidSwinkels.sh
+++ 04ScriptIngevdMondDavidSwinkels.sh
@@ -1,23 +1,19 @@
 #!bin/data
-echo "Script0rs"
+echo "Script0rs - Inge/David"
 echo "12 January 2017"
 echo "Calculate LandSat NDVI"
+clear
+cp LE71700552001036SGS00_SR_Gewata_INT1U.tif input.tif
 cd /home/ubuntu/data/
-fn=afbeelding.tif
-echo "The input file: $fn"
-outfn="ndvi.tif"
-echo "The output file: $outfn"
 echo "calculate ndvi"
-gdal_calc.py -A $fn --A_band=4 -B $fn --B_band=3 --outfile=$outfn --calc="(A.astype(float)-B)/(A.astype(float)+B)" --type='Float32'
+gdal_calc.py -A $fn --A_band=4 -B input.tif --B_band=3 --outfile=ndvi.tif --calc="(A.astype(float)-B)/(A.astype(float)+B)" --type='Float32'
 echo "resampling the NDVI tif file to pixels of 60m"
-fntemp="resampledndvi.tif"
-gdal_translate -tr 60 60 $outfn $fntemp
+gdal_translate -tr 60 60 ndvi.tif resampledndvi.tif
 echo "reprojecting the resampled file to Lat/Long WGS84"
-fnout="reprojectedndvi.tif"
-gdalwarp -t_srs EPSG:4326 $fntemp $fnout
+gdalwarp -t_srs EPSG:4326 resampledndvi.tif reprojectedndvi.tif
 echo "look at some histogram statistics"
-gdalinfo -hist -stats $fnout
-rm -f $fntemp
+gdalinfo -hist -stats ndvi.tif
+rm -f resampledndvi.tif
 
 
 
